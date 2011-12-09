b62272cf82c4ac7335f31bb3054fd02eda3f4f9b
Jacky Alcine <jackyalcine@gmail.com>
2011-12-08 19:27:35 -0500

    Updated build system.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
-#include <kapplication.h>
+/**
+ * This file is part of SpeechControl
+ *
+ * Copyright 2011 Jacky Alcine <jacky.alcine@thesii.org>
+ *
+ * SpeechControl is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * SpeechControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with SpeechControl; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-int main(int argc, char** argv){
+#include "core.hpp"
+
+int main( int argc, char** argv ) {
   return 0;
 }
