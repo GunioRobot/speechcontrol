8e2e77e4547cd52edb8e5e9f222351ed30b26857
Jacky Alcine <jackyalcine@gmail.com>
2011-12-14 18:09:58 -0500

    Fixed build script.

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #/bin/bash
 echo "Downloading packages...."
-sudo apt-get install libqt4-dev cmake git
+sudo apt-get install libqt4-dev cmake
 
 echo "Cleaning..."
 rm spchcntrl -rvf
 
 echo "Obtaining code..."
-git clone git://github.com/spchcntrl/speechcontrol spchcntrl
+wget -c https://github.com/spchcntrl/speechcontrol/tarball/master
+tar -xvf master
 
 echo "Building..."
-cd spchcntrl
-mkdir build
+cd spchcntrl*
+mkdir build -p
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
 
